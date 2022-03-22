b9cbf1596de36c0f44ae488d6930a341952d6ec9
Felipe Figueiredo <philsf79@gmail.com>
2022-03-22 17:55:27 -0300

    protótipo

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -1,6 +1,6 @@
 # setup -------------------------------------------------------------------
 # library(moderndive)
-# library(broom)
+library(broom)
 # library(lmerTest)
 # library(broom.mixed)
 # library(simputation)
@@ -8,6 +8,18 @@
 
 # raw estimate ------------------------------------------------------------
 
+mod_crude <- glm(outcome ~ group, family = binomial, analytical)
 
 # adjusted ----------------------------------------------------------------
+
+mod_full <- glm(outcome ~ group + sexo + has + asa + dm + tabagismo, family = binomial, analytical)
+
+anova(mod_crude, mod_full, test = "Chisq")
+mod_full %>% tidy
+mod_full %>% augment
+
+tab_inf <- tbl_merge(list(
+  mod_crude %>% tbl_regression(exp = TRUE),
+  mod_full %>% tbl_regression(exp = TRUE)
+))
 
